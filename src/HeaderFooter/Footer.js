3bc6b3e8f9e16d2c6e83ac1e853b91f14e016552
Rashed Abdullah <maarashed40@gmail.com>
2023-09-26 20:57:18 +0600

    added new items

---
+++ src/HeaderFooter/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = (props) => {
+  return (
+    <div className='footer'>
+        <div className='footerOne'>
+            <h3 className='text-center pt-2'>More...</h3>
+
+        </div>
+        <div className='allUL'>
+            <ul className='pt-3'>
+                <li><a href="">Vedio Editing</a></li>
+                <li><a href="">Graphic Designing</a></li>
+                <li><a href="">Web development</a></li>
+                <li><a href="">Professonal works</a></li>
+            </ul>
+
+            <ul className='pt-3'>
+                <li><a href="">Vedio Editing</a></li>
+                <li><a href="">Graphic Designing</a></li>
+                <li><a href="">Web development</a></li>
+                <li><a href="">Professonal works</a></li>
+            </ul>
+
+            <ul className='pt-3'>
+                <li><a href="">Vedio Editing</a></li>
+                <li><a href="">Graphic Designing</a></li>
+                <li><a href="">Graphic Designing</a></li>
+                <li><a href="">Web development</a></li>
+                <li><a href="">Professonal works</a></li>
+            </ul>
+
+            <ul className='pt-3'>
+                <li><a href="">Vedio Editing</a></li>
+                <li><a href="">Graphic Designing</a></li>
+                <li><a href="">Graphic Designing</a></li>
+                <li><a href="">Web development</a></li>
+                <li><a href="">Professonal works</a></li>
+            </ul>
+
+            <div className='pt-3'>
+                <p>Rashed Abdullah</p>
+            </div>
+        </div>
+        <div className='pt-3 pb-3'>
+            <p className='text-center'><code> &#169; All Right Reserved by Rashed Abdullah</code></p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Header And Footer:
+import Header from './HeaderFooter/Header'
+import Footer from './HeaderFooter/Footer'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
